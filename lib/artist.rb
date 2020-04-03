6de8c3ef943b820d265b2fe98f5e38391d0a4267
Jordan Romero <jordan.t.romero@gmail.com>
2020-04-03 20:38:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,54 +1,32 @@
 require 'pry'
 
-class Artist
-  attr_accessor :name	  attr_accessor :name
-  attr_reader :songs	  attr_reader :songs
-
+lass Artist
+  attr_accessor :name
+  attr_reader :songs
 
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  include Paramable::InstanceMethods
 
-  @@artists = []	  @@artists = []
+  @@artists = []
+  
+  def self.all
+    @@artists
+  end
 
-
-  def initialize	  def initialize
-    @@artists << self	    # @@artists << self
-    # self.class.all << self will be replaced by super
+  def initialize
     super
-    @songs = []	    @songs = []
-  end	  end
+    @songs = []
+  end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
+  end
 
-  def self.find_by_name(name)	  # def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}	  #   @@artists.detect{|a| a.name == name}
-  end	  # end
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
 
-
-  def self.all	  def self.all
-    @@artists	    @@artists
-  end	  end
-
-
-  def self.reset_all	  # def self.reset_all
-    self.all.clear	  #   self.all.clear
-  end	  # end
-
-
-  def self.count	  # def self.count
-    self.all.count	  #   self.all.count
-  end	  # end
-
-
-  def add_song(song)	  def add_song(song)
-    @songs << song	    @songs << song
-@@ -36,7 +43,7 @@ def add_songs(songs)
-    songs.each { |song| add_song(song) }	    songs.each { |song| add_song(song) }
-  end	  end
-
-
-  def to_param	  # def to_param
-    name.downcase.gsub(' ', '-')	  #   name.downcase.gsub(' ', '-')
-  end	  # end
-end	end
+end
