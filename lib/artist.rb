8c804fcfa0f9686160b24c8f7f2be90683e8792a
Jordan Romero <jordan.t.romero@gmail.com>
2020-04-03 20:29:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,14 +22,6 @@ class Artist
     @@artists
   end
 
-  #def self.reset_all
-   # self.all.clear
-  #end
-
-  #def self.count
-   # self.all.count
-  #end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -39,7 +31,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
